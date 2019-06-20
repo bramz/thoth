18f08c1a9186a8c26bf7edc150e576c3a2dc887f
bramz <brockramz@gmail.com>
2019-06-19 21:36:56 -0400

    Stub out templates and routing, resolves, #11 and #12

--- ctrl/__main__.py
+++ ctrl/__main__.py
@@ -1,6 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, url_for
+from flask_cachebuster import CacheBuster
+
+app = Flask(__name__, template_folder='templates', static_folder='assets')
+config = { 'extensions': ['.js', '.css', '.csv'], 'hash_size': 5 }
+
 
 @app.route("/")
 def index():
-    return "Welcome to Thoth!"
+    return render_template('index.html')
+
+
+@app.route("/login", methods=('GET', 'POST'))
+def login():
+    return render_template('login.html')
+
+
+@app.route("/register", methods=('GET', 'POST'))
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    cache_buster = CacheBuster(config=config)
+    cache_buster.init_app(app)
+    app.run(debug = True)
